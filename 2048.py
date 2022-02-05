ba9159dcf2cf525f390fc67bfcb7d54ceec57801
ashwindamam <66587716+ashwindamam@users.noreply.github.com>
2022-02-06 01:59:05 +0530

    Add addDefault function

--- 2048.py
+++ 2048.py
@@ -1,3 +1,6 @@
+import random
+
+
 def displayBoard(board):
     for line in board:
         print(*line, end="\n")
@@ -6,7 +9,20 @@ def displayBoard(board):
 def startPlay():
     board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     print("-----------2048 GAME--------------")
+    addDefault(board)
+    addDefault(board)
     displayBoard(board)
+
+def addDefault(board):
+    r = random.randint(0, 3)
+    c = random.randint(0, 3)
+    default = random.randint(1, 2) * 2
+
+    if board[r][c] == 0:
+        board[r][c] = default
+    else:
+        addDefault(board)
+
 
 def main():
     startPlay()
