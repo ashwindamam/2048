fb3be3e3645ddb9488e903b83724233ad914fe10
ashwindamam <66587716+ashwindamam@users.noreply.github.com>
2022-02-06 02:23:34 +0530

    Add swipe up and down funcionalities

--- 2048.py
+++ 2048.py
@@ -1,8 +1,6 @@
 import random
 
-
 board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-
 
 def displayBoard():
     addDefault()
@@ -11,20 +9,30 @@ def displayBoard():
 
 
 def startPlay():
-    
     print("-----------2048 GAME--------------")
     addDefault()
     displayBoard()
     win = True
     while win:
-        print("\nFollowing are the directions: 1=LEFT , 2=RIGHT ")
-        direction = int(input("what is your direction\t"))
-        if direction == 1:
-            sumLeft()
-            displayBoard()
-        elif direction == 2:
-            sumRight()
-            displayBoard()
+        try:
+            print("\nFollowing are the directions: 1=LEFT , 2=RIGHT , 3=UP , 4=DOWN")
+            direction = int(input("what is your direction\t"))
+            if direction == 1:
+                sumLeft()
+                displayBoard()
+            elif direction == 2:
+                sumRight()
+                displayBoard()
+            elif direction == 3:
+                sumUp()
+                displayBoard()
+            elif direction == 4:
+                sumDown()
+                displayBoard()
+            else:
+                print("Enter no. between 1-4")
+        except ValueError:
+            print("WRONG INPUT. PLEASE TYPE AGAIN!")
 
 
 def addDefault():
@@ -80,6 +88,48 @@ def sumRight():
     sumLeft()
     for i in range(4):
         board[i].reverse()
+
+
+def swipeDown():
+    for i in range(2, -1, -1):
+        for j in range(4):
+            if board[i][j] != 0 and i == 2:
+                if board[3][j] == 0:
+                    board[3][j] = board[i][j]
+                    board[i][j] = 0
+            if board[i][j] != 0 and i == 1:
+                if board[2][j] == 0:
+                    board[2][j] = board[i][j]
+                    board[i][j] = 0
+                if board[3][j] == 0:
+                    board[3][j] = board[2][j]
+                    board[2][j] = 0
+            if board[i][j] != 0 and i == 0:
+                if board[1][j] == 0:
+                    board[1][j] = board[i][j]
+                    board[i][j] = 0
+                if board[2][j] == 0:
+                    board[2][j] = board[1][j]
+                    board[1][j] = 0
+                if board[3][j] == 0:
+                    board[3][j] = board[2][j]
+                    board[2][j] = 0
+
+
+def sumDown():
+    swipeDown()
+    for i in range(2, -1, -1):
+        for j in range(4):
+            if board[i][j] == board[i + 1][j]:
+                board[i + 1][j] = board[i][j] + board[i + 1][j]
+                board[i][j] = 0
+    swipeDown()
+
+
+def sumUp():
+    board.reverse()
+    sumDown()
+    board.reverse()
 
 
 def main():
